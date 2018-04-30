658a494c0c541151ac22ca87ebb5ed99c83ed4b0
ccdle12 <chris.coverdale24@gmail.com>
2018-04-30 10:26:17 +0100

    Refactored Point

--- Point/Point.cpp
+++ Point/Point.cpp
@@ -3,64 +3,64 @@
 #include <cmath>
 
 // constructor
-Point::Point(FieldElement x, FieldElement y, FieldElement a, FieldElement b)
+Point::Point(FieldElement *x, FieldElement *y, FieldElement *a, FieldElement *b)
 {
 	// unsure how to handle point at infinity
-	if (x.getNum() == NULL && y.getNum() == NULL)
-		throw std::runtime_error("Poiny at Infinity");
+	// if (x.getNum() == NULL && y.getNum() == NULL)
+	// 	throw std::runtime_error("Poiny at Infinity");
 	
-	if (y.powers(2) != x.powers(3) + (a * x) + b)
-		throw std::runtime_error("Point not on curve");
+	// if (y.powers(2) != x.powers(3) + (a * x) + b)
+	// 	throw std::runtime_error("Point not on curve");
 
-    this->x = x;
-    this->y = y;
-    this->a = a;
-    this->b = b;
+	mX = x;
+    mY = y;
+    mA = a;
+    mB = b;
 };
 
 // setter and getters
-FieldElement Point::getX() { return this->x; };
-FieldElement Point::getY() { return this->y; };
+FieldElement* Point::getX() { return this->mX; };
+FieldElement* Point::getY() { return this->mY; };
 
-// arithmetic
-bool Point::operator==(Point p2)
-{
-	return this->x == p2.x && this->y == p2.y && this->a == p2.a && this->b == p2.b;
-};
+// // arithmetic
+// bool Point::operator==(Point p2)
+// {
+// 	return this->x == p2.x && this->y == p2.y && this->a == p2.a && this->b == p2.b;
+// };
 
-bool Point::operator!=(Point p2)
-{
-	return this->x == p2.x && this->y == p2.y && this->a == p2.a && this->b == p2.b ? false : true;
-};
+// bool Point::operator!=(Point p2)
+// {
+// 	return this->x == p2.x && this->y == p2.y && this->a == p2.a && this->b == p2.b ? false : true;
+// };
 
-Point Point::operator+(Point p2)
-{
-	if (this->a != p2.a || this->b != p2.b)
-		throw std::runtime_error("Points are using different curves");
+// Point Point::operator+(Point p2)
+// {
+// 	if (this->a != p2.a || this->b != p2.b)
+// 		throw std::runtime_error("Points are using different curves");
 
-	if (this->x != p2.getX())
-	{
-		FieldElement slope = (p2.getY() - this->y) / (p2.getX() - this->x);
+// 	if (this->x != p2.getX())
+// 	{
+// 		FieldElement slope = (p2.getY() - this->y) / (p2.getX() - this->x);
 
-		// slope^2 - x1 - x2
-		FieldElement x3 = slope.powers(2) - this->x - p2.getX();
+// 		// slope^2 - x1 - x2
+// 		FieldElement x3 = slope.powers(2) - this->x - p2.getX();
 
-		// slope * (x1 - x3) - y1
-		FieldElement y3 = slope * (this->x - x3) - this->y;
+// 		// slope * (x1 - x3) - y1
+// 		FieldElement y3 = slope * (this->x - x3) - this->y;
 
-		return Point(x3, y3, this->a, this->b);
-	} else {
-		// point doubling
-		// (3 * x1^2 + a) / (2 * y1)
-		// Hacky solution to using prime for FieldElement
-		FieldElement slope = (FieldElement(3, this->x.getPrime()) * this->x.powers(2) + this->a) / (FieldElement(2, this->x.getNum()) * this->y);
+// 		return Point(&x3, &y3, &this->a, &this->b);
+// 	} else {
+// 		// point doubling
+// 		// (3 * x1^2 + a) / (2 * y1)
+// 		// Hacky solution to using prime for FieldElement
+// 		FieldElement slope = (FieldElement(3, this->x.getPrime()) * this->x.powers(2) + this->a) / (FieldElement(2, this->x.getNum()) * this->y);
 
-		// slope^2 - 2 * x1
-		FieldElement x3 = slope.powers(2) - FieldElement(2, this->x.getPrime()) * this->x;
+// 		// slope^2 - 2 * x1
+// 		FieldElement x3 = slope.powers(2) - FieldElement(2, this->x.getPrime()) * this->x;
 
-		// slope * (x1 - x3) - y1
-		FieldElement y3 = slope * (this->x - x3) - this->y;
+// 		// slope * (x1 - x3) - y1
+// 		FieldElement y3 = slope * (this->x - x3) - this->y;
 
-		return Point(x3, y3, this->a, this->b);
-	}
-};
+// 		return Point(&x3, &y3, &this->a, &this->b);
+// 	}
+// };
--- Point/Point.h
+++ Point/Point.h
@@ -1,20 +1,26 @@
-#include "../FieldElement/FieldElement.cpp"
+#include "../FieldElement/FieldElement.h"
 
 class Point 
 {
     private:
-        FieldElement x, y, a, b;
+        FieldElement *mX;
+        FieldElement *mY; 
+        FieldElement *mA; 
+        FieldElement *mB;
 
     public:
         // constructor
-        Point(FieldElement x, FieldElement y, FieldElement a, FieldElement b);
+        Point(FieldElement *x, FieldElement *y, FieldElement *a, FieldElement *b);
+
+        // overload constructor
+        // Point(const int _x, const int _y, const int _a, const int _b, const int prime): x(_x, prime), y(_y, prime), a(_a, prime), b(_b, prime) {};
 
         // setter and getters
-        FieldElement getX();
-        FieldElement getY();
+        FieldElement* getX();
+        FieldElement* getY();
 
         // arithmetic
-        bool operator==(Point p2);
-        bool operator!=(Point p2);
-        Point operator+(Point p2);
+        // bool operator==(Point p2);
+        // bool operator!=(Point p2);
+        // Point operator+(Point p2);
 };
